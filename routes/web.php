44816947d50e05951a30b5565a80a5e9d757f25c
Artem <k3koffer@yandex.ru>
2025-09-23 14:51:17 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 
+Route::domain('blog.neboisyaznat.ru')->group(function () {
+    Route::get('/', [BlogController::class, 'index']);
+    Route::get('/main', function () {
+        return redirect()->away('https://neboisyaznat.ru');
+    });
+    Route::get('/posts/{post:link}', [BlogController::class, 'show'])->name('posts.show');
+});
+
 Route::domain('neboisyaznat.ru')->group(function () {
     Route::get('/', [HomeController::class, 'index']);
     Route::get('/about', [HomeController::class, 'about']);
@@ -15,14 +23,6 @@ Route::domain('neboisyaznat.ru')->group(function () {
         return redirect()->away('http://blog.neboisyaznat.ru');
     });
     Route::post('/reviews', [HomeController::class, 'storeReview']);
-});
-
-Route::domain('blog.neboisyaznat.ru')->group(function () {
-    Route::get('/', [BlogController::class, 'index']);
-    Route::get('/main', function () {
-        return redirect()->away('https://neboisyaznat.ru');
-    });
-    Route::get('/posts/{post:link}', [BlogController::class, 'show'])->name('posts.show');
 });
 
 // Route::domain('edu.nbzhub.org')->group(function () {
