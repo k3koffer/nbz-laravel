30c99d970813f51d14ffd53ff7ded3c4a2c458a3
Artem <k3koffer@yandex.ru>
2025-09-23 14:29:17 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\App;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 
-if (App::environment('production')) {
-    $mainDomain = 'neboisyaznat.ru';
-    $blogDomain = 'blog.neboisyaznat.ru';
-} else {
-    $mainDomain = config('app.main_domain');
-    $blogDomain = config('app.blog_domain');
-}
-
-Route::domain($mainDomain)->group(function () {
+Route::domain(config('app.main_domain'))->group(function () {
     Route::get('/', [HomeController::class, 'index']);
     Route::get('/about', [HomeController::class, 'about']);
     Route::get('/reviews', [HomeController::class, 'reviews']);
@@ -26,7 +17,7 @@ Route::domain($mainDomain)->group(function () {
     Route::post('/reviews', [HomeController::class, 'storeReview']);
 });
 
-Route::domain($blogDomain)->group(function () {
+Route::domain(config('app.blog_domain'))->group(function () {
     Route::get('/', [BlogController::class, 'index']);
     Route::get('/main', function () {
         return redirect()->away('http://' . config('app.main_domain'));
